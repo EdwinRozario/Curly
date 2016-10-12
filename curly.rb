58ee54c2baadc9d98565f906f2e701a8a5dce105
Edwin Rozario <edwin@boost.co.nz>
2016-10-13 11:51:51 +1300

    Updated make_params to create [] queries for nested hashes

--- curly.rb
+++ curly.rb
@@ -31,14 +31,23 @@ class Curly
   end
 
   def make_params(params)
+    nested_query = ''
+    params.each do |param, value|
+      if value.is_a? Hash
+        params.delete(param)
+        nested_query += "&#{param}\\[#{value.keys.first}\\]=#{value[value.keys.first]}"
+      end
+    end
+
     uri = Addressable::URI.new
     uri.query_values = params
-    uri.query
+    nested_query.empty? ? uri.query : "#{uri.query}#{nested_query}"
   end
 
   def call(data, method)
     response = `curl -s #{data}`
     puts "#{method}: #{data}"
+
     response_value = JSON.parse(response)
     puts "RESPONSE: #{response_value}"
     p '-------------------------------------------------------------------------------------------------------------------------'
@@ -46,39 +55,52 @@ class Curly
   end
 end
 
-curly = Curly.new('http://localhost:3000', :json, api_key: 'RYAVDhaqNsCyyYtssLFE')
+# curly = Curly.new('http://localhost:3000', :json, api_key: 'RYAVDhaqNsCyyYtssLFE')
+
+curly = Curly.new('http://api.dnz0a.digitalnz.org', :json, api_key: '387d16c7596c9cfff190d14a81ec189a')
+
+# curly.post('stories/57fe9883297b1f1f63000630/story_items',
+#            { block: { type: 'text', sub_type: 'heading',
+#                           content: { value: 'Value text here' },
+#                           meta: {} } })
+
+curly.get('records', { and: { category: 'Images' } })
+
 
 # TEST CALLS
-curly.get('stories')
-curly.get('stories/57fd697769ae851c55000000')
-curly.get('stories/57fd697769ae851c55000000/story_items')
-curly.get('stories/57fd697769ae851c55000000/story_items/57fd885169ae851eed000000')
-curly.post('stories', { name: 'New Story For Test'})
-curly.post('stories/57fd697769ae851c55000000/story_items', {})
-curly.post('stories/57fd697769ae851c55000000/story_items', { block: {}})
-curly.post('stories/57fd697769ae851c55000000/story_items',
-           { block: { type: 'texting', sub_type: 'header' } })
+# curly.get('stories')
+# curly.get('stories/57fd697769ae851c55000000')
+# curly.get('stories/57fd697769ae851c55000000/story_items')
+# curly.get('stories/57fd697769ae851c55000000/story_items/57fd885169ae851eed000000')
+# curly.post('stories', { name: 'New Story For Test'})
+# curly.post('stories/57fd697769ae851c55000000/story_items', {})
+# curly.post('stories/57fd697769ae851c55000000/story_items', { block: {}})
+# curly.post('stories/57fd697769ae851c55000000/story_items',
+#            { block: { type: 'texting', sub_type: 'header' } })
 
-curly.post('stories/57fd697769ae851c55000000/story_items',
-           { block: { type: 'text', sub_type: 'heading' } })
+# curly.post('stories/57fd697769ae851c55000000/story_items',
+#            { block: { type: 'text', sub_type: 'heading' } })
 
-curly.post('stories/57fd697769ae851c55000000/story_items',
-           { block: { type: 'text', sub_type: 'heading',
-                          content: {}, meta: {} } })
+# curly.post('stories/57fd697769ae851c55000000/story_items',
+#            { block: { type: 'text', sub_type: 'heading',
+#                           content: {}, meta: {} } })
 
-curly.post('stories/57fd697769ae851c55000000/story_items',
-           { block: { type: 'text', sub_type: 'heading',
-                          content: { value: 'Value text here' },
-                          meta: {} } })
+# curly.post('stories/57fd697769ae851c55000000/story_items',
+#            { block: { type: 'text', sub_type: 'heading',
+#                           content: { value: 'Value text here' },
+#                           meta: {} } })
 
-curly.patch('stories/57fd697769ae851c55000000/story_items/57fd885169ae851eed000000', {
-  item: { type: 'text', sub_type: 'heading',
-                          content: { value: 'Updated Text Again' },
-                          meta: {} }
-  })
+# curly.patch('stories/57fd697769ae851c55000000/story_items/57fd885169ae851eed000000', {
+#   item: { type: 'text', sub_type: 'heading',
+#                           content: { value: 'Updated Text Again' },
+#                           meta: {} }
+#   })
 
 
-
+# curly.post('stories/57fe9883297b1f1f63000630/story_items',
+#            { block: { type: 'text', sub_type: 'heading',
+#                           content: { value: 'This is the new heading' },
+#                           meta: {} } })
 
 
 
