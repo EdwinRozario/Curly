dea3c3100b34fb437a77fa165b21e602b2ad7c07
edwinrozario <rozarioed@gmail.com>
2016-10-17 22:25:22 +1300

    - Added Gemfile - Added terminal print feature

--- README.md
+++ README.md
@@ -5,24 +5,25 @@ Curly is a class that enables to make curl requests for GET and POST methods.
 ## Usage
 
 ```
-curly = Curly.new('http://localhost:3000', :json, api_key: 'RYAVDhaqNsCyyYtssLFE')
-# initialise the object with the domain name, fomat and any parameter thats used with 
+curly = Curly.new('http://localhost:3000', true, :json, api_key: 'anApiKeey')
+# initialise the object with the domain name, format and any parameter thats used with
+# second param is to enable pretty print response. By defult this vale is false 
 # all the url calls
 
 curly.get('posts')
-# This will make a GET call to 'http://localhost:3000/posts.json?api_key=RYAVDhaqNsCyyYtssLFE'
+# This will make a GET call to 'http://localhost:3000/posts.json?api_key=anApiKeey'
 
 curly.get('posts', { user: 'Yukava' })
-# This will make a GET call to 'http://localhost:3000/posts.json?api_key=RYAVDhaqNsCyyYtssLFE&user=Yukava'
+# This will make a GET call to 'http://localhost:3000/posts.json?api_key=anApiKeey&user=Yukava'
 
 curly.get('posts/126/comments')
-# This will make a GET call to 'http://localhost:3000/posts/126/comments.json?api_key=RYAVDhaqNsCyyYtssLFE'
+# This will make a GET call to 'http://localhost:3000/posts/126/comments.json?api_key=anApiKeey'
 
 curly.get('posts', { and: { category: 'Images' } })
-# This will make a GET call to 'http://localhost:3000/posts.json?api_key=RYAVDhaqNsCyyYtssLFE&and[category]=Image'
+# This will make a GET call to 'http://localhost:3000/posts.json?api_key=anApiKeey&and[category]=Image'
 
 curly.post('posts', { name: 'Foo', description: 'Bar', tag: 'Quxx' })
-# This will make a POST call to 'http://localhost:3000/posts.json?api_key=RYAVDhaqNsCyyYtssLFE&name=Foo,description=Bar&tag=Quxx'
+# This will make a POST call to 'http://localhost:3000/posts.json?api_key=anApiKeey&name=Foo,description=Bar&tag=Quxx'
 ```
 
 Note: More work to be done in future
--- curly.rb
+++ curly.rb
@@ -2,32 +2,41 @@
 require 'addressable/uri'
 require 'json'
 require 'pry'
+require 'bundler/setup'
+Bundler.require
 
 # Curly class
 # Initialize with the domain name, fomart and any common parameter
 # used in every api call like an api_key
 class Curly
-  attr_accessor :domain, :format, :constant_params
+  attr_accessor :domain, :format, :constant_params, :pretty
 
-  def initialize(domain, format, params)
+  def initialize(domain, format, pretty = false, params)
     @domain = domain
     @format = format
     @constant_params = params
+    @pretty = pretty
   end
 
   def get(path, params = {})
     complete_url = "'#{domain}/#{path}.#{format}?#{make_params(constant_params.merge(params))}'"
-    call(complete_url, 'GET')
+    response = call(complete_url)
+    printer(method: 'GET', url: complete_url,
+            response: response)
   end
 
   def post(path, data)
     complete_url = "-X POST -H 'Content-Type: application/json' -d '#{data.to_json}' '#{domain}/#{path}.#{format}?#{make_params(constant_params)}'"
-    call(complete_url, 'POST')
+    response = call(complete_url)
+    printer(method: 'POST', url: complete_url,
+            data: data.to_s, response: response)
   end
 
   def patch(path, data)
     complete_url = "-X PATCH -H 'Content-Type: application/json' -d '#{data.to_json}' '#{domain}/#{path}.#{format}?#{make_params(constant_params)}'"
-    call(complete_url, 'PATCH')
+    response = call(complete_url)
+    printer(method: 'PATCH', url: complete_url,
+            data: data.to_s, response: response)    
   end
 
   def make_params(params)
@@ -44,15 +53,30 @@ class Curly
     nested_query.empty? ? uri.query : "#{uri.query}#{nested_query}"
   end
 
-  def call(data, method)
-    response = `curl -s #{data}`
-    puts "#{method}: #{data}"
+  def printer(method: nil, url: nil, data: nil, response: nil)
+    table = Terminal::Table.new do |t|
+      t << ['Method', method]
+      t << :separator
+      t.add_row ['Url', url]
+      if data
+        t.add_separator
+        t.add_row ['Data', data]
+      end
+    end
 
-    response_value = JSON.parse(response)
-    puts "RESPONSE: #{response_value}"
-    p '-------------------------------------------------------------------------------------------------------------------------'
-    response_value
+    puts table
+    puts '  Response'
+    puts '-----------'
+    puts pretty ? JSON.pretty_generate(response) : response
+  end
+
+  def call(data)
+    response = `curl -s #{data}`
+    JSON.parse(response)
   end
 end
+
+curly = Curly.new('http://api.dnz0a.digitalnz.org', :json, true, api_key: '387d16c7596c9cfff190d14a81ec189a')
+curly.get('records/32058904')
 
 
